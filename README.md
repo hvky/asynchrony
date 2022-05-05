e8a9bb0142a8cb9645327311bbe8908e2cdac74d
Eddy <eddysejas37@gmail.com>
2022-05-04 22:34:13 -0600

    -

---
+++ README.md
@@ -0,0 +1,3 @@
+# ASYNCHRONY
+
+Resolucion de un problema utilizando callbacks, promise y asyn-await.
---
+++ src/promise/index.js
@@ -0,0 +1,38 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("Hey!");
+    } else {
+      reject("Ups!");
+    }
+  });
+};
+
+somethingWillHappen()
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
+
+//////////////////////////////////////////////////////////
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve("True");
+      }, 2000);
+    } else {
+      const error = new Error("Ups!");
+      reject(error);
+    }
+  });
+};
+
+somethingWillHappen2()
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
+
+//////////////////////////////////////////////////////////
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then((response) => console.log("Array of result", response))
+  .catch(err => console.error(err));
